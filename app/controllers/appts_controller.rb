cd3bf115cd266e06a9ca1f25129aecef5da70628
Avi Charlop <avicharlop@gmail.com>
2016-05-31 17:12:42 -0400

    create actions

--- app/controllers/appts_controller.rb
+++ app/controllers/appts_controller.rb
@@ -1,2 +1,34 @@
 class ApptsController < ApplicationController
+	
+	def index
+		@appts = Appt.all
+		render json: @appts
+	end
+
+
+
+	def create
+		@appt = Appt.new(appt_params)
+	end
+
+
+
+	def update
+		@appt = Appt.by_id(params[:id])
+		@appt.update(appt_params)
+	end
+
+
+
+	def destroy
+		@appt = Appt.by_id(params[:id]).destroy
+	end
+
+
+	
+	private
+
+	def appt_params
+		params.require(:appt).permit(:start_time, :end_time, :first_name, :last_name, :comments)
+	end
 end
