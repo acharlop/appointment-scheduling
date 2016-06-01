878ba35a0ae1e4d7e4262c0336e60d46295552f1
Avi Charlop <avicharlop@gmail.com>
2016-05-31 22:59:20 -0400

    creation validation added

--- app/models/appt.rb
+++ app/models/appt.rb
@@ -1,5 +1,21 @@
 class Appt < ActiveRecord::Base
+
+	validates :start_time, :end_time, :first_name, :last_name, presence: true
+
+	validate :appt_in_the_future
+
+
 	def self.by_id id
 		self.find_by(id: id)
+	end
+
+
+
+	private
+
+	def appt_in_the_future
+		errors.add(:start_time, "must be in the future") unless start_time > Time.now
+		errors.add(:end_time, 	"must be in the future") unless end_time 	 > Time.now
+		errors.add(:end_time, 	"must be after start time") unless end_time > start_time
 	end
 end
