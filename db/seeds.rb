024de3ac61dd9ef3855c0c359d722876f1023ef7
Avi Charlop <avicharlop@gmail.com>
2016-05-31 22:07:05 -0400

    fixed csv date parsing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 require "csv"
 
+def csv_date(str)
+	DateTime.strptime(str,"%m/%d/%y %H:%M")
+end
 
 CSV.foreach("app/assets/appt_data.csv", {headers: true}) do |appt|
 	Appt.create(
-		start_time: appt["start_time"],
-		end_time: 	appt["end_time"],
+		start_time: csv_date(appt["start_time"]),
+		end_time: 	csv_date(appt["end_time"]),
 		first_name: appt["first_name"],
 		last_name: 	appt["last_name"],
 		comments: 	appt["comments"]
