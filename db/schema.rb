d338fde208f793e610cfb78ca1f3614aa65090d6
Avi Charlop <avicharlop@gmail.com>
2016-06-01 15:01:35 -0400

    moving from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160531203956) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appts", force: :cascade do |t|
     t.datetime "start_time"
     t.datetime "end_time"
